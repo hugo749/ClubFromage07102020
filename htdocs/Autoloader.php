bd10184acb09860661801d952d876e863c4cd0cd
hugo <hugofeige@hotmail.fr>
2020-10-07 11:40:36 +0200

    autoloader3

--- htdocs/Autoloader.php
+++ htdocs/Autoloader.php
@@ -7,11 +7,14 @@ class Autoloader{
     }
     static function autoload($business){
 
+
         //require 'Model/business/'.$business.'.php';
-        $file = str_replace('\\', '/', $business);
-        $file = '/'. $file . '.php';
-        echo $file;
-//        if(file_exists($file))
+        $file = str_replace('ClubFromage', '/', $business);
+        $file = 'C:/Bitnami/wampstack-7.4.6-1/apps/ClubFromage/htdocs/'. $file . '.php';
+
+
+
+        if(file_exists($file))
             return require $file;
 
     }
--- htdocs/index.php
+++ htdocs/index.php
@@ -5,14 +5,15 @@ require('Model\business\pays.php');
 require('Model\business\avis.php');*/
 
 
-require 'Model/Autoloader.php';
+require_once 'Autoloader.php';
 Autoloader::register();
 
 
-/*use ClubFromage\Model\business\fromage;
+
+use ClubFromage\Model\business\fromage;
 use ClubFromage\Model\business\membre;
 use ClubFromage\Model\business\pays;
-use ClubFromage\Model\business\avis;*/
+use ClubFromage\Model\business\avis;
 
 /*$fromage = new fromage();
 $fromage->setNom("Raclette");
@@ -35,22 +36,27 @@ echo '<br>';
 $avis = new avis();
 $avis->setAvis("je suis super content de ce fromage");
 $avis->getAvis();
-echo $avis->getAvis();*/
-
+echo $avis->getAvis();
+*/
 
 
 $fromage = array("id"=>1,"nom"=>"raclette", "affinage"=>3, "DateDeCrea" => "02/11/2015", "Image"=>"beau", "recette"=>"Pour raclette entre amis", "histoire"=>"viens de la Yaute" );
-$fromage1 = new \ClubFromage\Model\business\fromage($fromage);
+$fromage1 = new fromage($fromage);
+echo 'Voici la date de création du fromage: ';
 echo $fromage1->getDateDeCrea();
 echo "<br>";
+
 $pays = array("id"=>1, "nom"=>"France");
 $pays1= new pays($pays);
+echo 'Voici le nom du pays où il se trouve: ';
 echo $pays1->getNom();
 echo "<br>";
 $avis = array("avis"=>"super bon","note"=>"5/5" );
 $avis1= new avis($avis);
+echo 'Voici la note pour du fromage donné par un membre: ';
 echo $avis1->getNote();
 echo "<br>";
 $membre = array("id"=>1, "nom"=>"Feige", "pseudo"=>"hugue", "mail"=>"uriuerg@rfr.fr", "mdp"=>"21mm12", "datedeco"=>"20/10/2019", "Dateentrer"=>"19/09/2019", "DesscriptionRole"=>"frsgfdrgdsgrsdrg", "pésent"=>"oui" );
 $membre1= new membre($membre);
+echo "Voici l'addresse mail du membre en question: " ;
 echo $membre1->getMail();
